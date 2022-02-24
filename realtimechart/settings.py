e2824d84556c496be002c1cd73b52eda7983f45b
jcscale <jcescala700@yahoo.com>
2022-02-24 16:50:18 +0800

    migrated to postgreSQL

--- realtimechart/settings.py
+++ realtimechart/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'realtimechart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'realtimechart',
+        'USER': 'postgres',
+        'PASSWORD': 'magicolor2728',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
